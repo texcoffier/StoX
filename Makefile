f62424987e3ce3de090b6e836092eef63ee4dc94
Thierry EXCOFFIER <thierry.excoffier@univ-lyon1.fr>
2018-06-20 22:26:36 +0200

    Disable functionalities from command line or URL

--- Makefile
+++ Makefile
@@ -7,7 +7,7 @@ run-tests:StoX.js
 run:xxx.py
 	python3 xxx.py
 
-xxx.py: $(SRC) RapydScript concatenate.py $(SRC:.py=.py.html)
+xxx.py: $(SRC) RapydScript concatenate.py $(SRC:.py=.py.html) required.py
 	./concatenate.py $(SRC) >xxx.py
 
 RapydScript:
@@ -18,6 +18,13 @@ RapydScript:
 StoX.js:xxx.py RapydScript
 	RapydScript/bin/rapydscript --prettify --bare xxx.py | \
 		sed -e 's/x.__class__.__name__/typeof(x)/'  >$@
+
+required.py: # xxx.py
+	@echo "Compute required.py. Wait a long time..."
+	( echo "required = {" ; \
+	for F in $(SRC:.py=) ; \
+	do (python3 xxx.py -$$F >/dev/null 2>&1 ; echo "'$$F':$$?,") ; \
+	done ; echo '"":0}') | sed 's/[.]py//' | tee $@
 
 clean:
 	-rm xxx* *.py.html
--- concatenate.py
+++ concatenate.py
@@ -6,7 +6,7 @@ import json
 functionalities = {}
 s = []
 for filename in sys.argv[1:]:
-        s.append('if selected("{}"):\n'.format(filename))
+        s.append('if selected("{}"):\n'.format(filename[:-3]))
         doc = ""
         with open(filename, 'r') as f:
                 state = 'first'
@@ -25,7 +25,20 @@ for filename in sys.argv[1:]:
                 functionalities[filename[:-3]] = doc
 
 print('''
+disabled_functionalities = []
+
 def selected(name):
+    try:
+        import re
+        if ('-' + name) in re.sys.argv:
+            disabled_functionalities.append(name)
+            return False
+    except:
+        if ('-' + name) in window.location.toString():
+            disabled_functionalities.append(name)
+            return False
     return True
 functionalities = ''', json.dumps(functionalities), ';')
+with open('required.py', 'r') as f:
+        print(f.read())
 print(''.join(s))
